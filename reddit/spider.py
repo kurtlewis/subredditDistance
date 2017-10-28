57465a70be8df2af56626565652b1c49a553c20d
Kurt Lewis <kurt.jlewis15@gmail.com>
2017-10-28 14:38:26 -0400

    Add reddit spider and add subreddit scraping

---
+++ reddit/spider.py
@@ -0,0 +1,35 @@
+import praw
+from logger import log
+import re
+
+
+class Spider:
+
+    def __init__(self):
+        self.reddit = object()
+        try:
+            self.reddit = praw.Reddit('subredditDistance')
+        except Error:
+            print('Make sure you have a praw.ini file defined with a bot' +
+                  'called "subredditDistance"')
+            print(Error)
+        self.log = log.Log('./reddit-spider.log', 500)
+        self.queue = list()
+        self.subredditRegex = re.compile('(/r/[a-zA-Z0-9_]*) ')
+
+    def crawl(self, startingPoint):
+        """ Crawls from a starting point, adding to the queue any websites it finds """
+        topPosts = self.reddit.subreddit(startingPoint).top(limit=10)
+        for post in topPosts:
+            post.comments.replace_more(threshold=5)
+            comments = post.comments.list()
+            for comment in comments:
+                self.extractSubreddit(comment.body)
+
+    def extractSubreddit(self, commentStr):
+        result = self.subredditRegex.match(commentStr)
+        if result is not None:
+            # Found a link to a subreddit
+            sub = result.group(0)
+            print(sub)
+
